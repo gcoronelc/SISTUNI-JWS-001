25341e0b48cdb04a160335b5e0fdf888c9f60b98
GustavoCoronel <GustavoCoronel>
2016-01-31 19:16:59 -0500

    Clase 03

---
+++ Tema05/Algo/src/pe/egcc/model/Cliente.java
@@ -0,0 +1,15 @@
+package pe.egcc.model;
+
+public class Cliente {
+
+	private String codigo;
+	private String paterno;
+	private String materno;
+	private String nombre;
+	private String dni;
+	private String ciudad;
+	private String direccion;
+	private String telefono;
+	private String email;
+
+}
---
+++ Tema06/WSClientCS/WSClientCS/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WSClientCS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.lblSuma = new System.Windows.Forms.Label();
+            this.lblResta = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número 1:";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(116, 23);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 20);
+            this.txtNum1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Número 2:";
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(116, 69);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 20);
+            this.txtNum2.TabIndex = 3;
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Location = new System.Drawing.Point(251, 23);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(125, 65);
+            this.btnProcesar.TabIndex = 4;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // lblSuma
+            // 
+            this.lblSuma.Location = new System.Drawing.Point(44, 146);
+            this.lblSuma.Name = "lblSuma";
+            this.lblSuma.Size = new System.Drawing.Size(332, 23);
+            this.lblSuma.TabIndex = 5;
+            this.lblSuma.Text = "label3";
+            // 
+            // lblResta
+            // 
+            this.lblResta.Location = new System.Drawing.Point(44, 188);
+            this.lblResta.Name = "lblResta";
+            this.lblResta.Size = new System.Drawing.Size(332, 23);
+            this.lblResta.TabIndex = 6;
+            this.lblResta.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 273);
+            this.Controls.Add(this.lblResta);
+            this.Controls.Add(this.lblSuma);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.Label lblSuma;
+        private System.Windows.Forms.Label lblResta;
+    }
+}
+
